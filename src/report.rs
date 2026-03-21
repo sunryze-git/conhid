5f9df4097f13dbe450cfa91153892af55550c9d4
sunryze <48110960+sunryze-git@users.noreply.github.com>
2026-03-21 11:49:36 -0400

    report fixes

--- src/report.rs
+++ src/report.rs
@@ -1,6 +1,8 @@
 use core::fmt;
 use std::io::Error;
 
+use evdev::{AbsoluteAxisCode, EventType, InputEvent, KeyCode, uinput::VirtualDevice};
+
 pub struct InputReport {
     pub counter: u32,
     pub buttons: Buttons,
@@ -126,7 +128,7 @@ impl MagData {
 
 pub struct MotionData {
     pub timestamp: u32,
-    pub temperature: i16,
+    temperature: i16,
     pub accel_x: i16,
     pub accel_y: i16,
     pub accel_z: i16,
@@ -148,6 +150,16 @@ impl MotionData {
             gyro_z: i16::from_le_bytes([b[16], b[17]]),
         }
     }
+
+    /// Returns temperature in Celsius
+    pub fn temperature(&self) -> f64 {
+        25f64 + (self.temperature as f64 / 127.0)
+    }
+
+    /// Returns a filtered quaternion
+    pub fn to_quat() {
+        todo!()
+    }
 }
 
 #[derive(Debug)]
@@ -167,6 +179,12 @@ impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{self:?}")
     }
+}
+
+fn dpad_to_hat(up: bool, down: bool, left: bool, right: bool) -> (i32, i32) {
+    let x = right as i32 - left as i32;
+    let y = down as i32 - up as i32;
+    (x, y)
 }
 
 impl InputReport {
@@ -195,6 +213,124 @@ impl InputReport {
             right_trigger: buf[0x3D],
         })
     }
+
+    pub fn emit_to_device(
+        &self,
+        device: &mut VirtualDevice,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let mut events: Vec<InputEvent> = Vec::new();
+
+        let b = &self.buttons;
+        let btn_map: &[(KeyCode, bool)] = &[
+            (KeyCode::BTN_SOUTH, b.b),
+            (KeyCode::BTN_EAST, b.a),
+            (KeyCode::BTN_NORTH, b.x),
+            (KeyCode::BTN_WEST, b.y),
+            (KeyCode::BTN_TL, b.l),
+            (KeyCode::BTN_TR, b.r),
+            (KeyCode::BTN_TL2, b.zl),
+            (KeyCode::BTN_TR2, b.zr),
+            (KeyCode::BTN_SELECT, b.minus),
+            (KeyCode::BTN_START, b.plus),
+            (KeyCode::BTN_THUMBL, b.left_stick),
+            (KeyCode::BTN_THUMBR, b.right_stick),
+            (KeyCode::BTN_MODE, b.home),
+            (KeyCode::BTN_TRIGGER_HAPPY1, b.capture),
+            (KeyCode::BTN_TRIGGER_HAPPY2, b.gl),
+            (KeyCode::BTN_TRIGGER_HAPPY3, b.gr),
+            (KeyCode::BTN_TRIGGER_HAPPY4, b.sll),
+            (KeyCode::BTN_TRIGGER_HAPPY5, b.slr),
+            (KeyCode::BTN_TRIGGER_HAPPY6, b.srl),
+            (KeyCode::BTN_TRIGGER_HAPPY7, b.srr),
+        ];
+        for (key, pressed) in btn_map {
+            events.push(InputEvent::new(EventType::KEY.0, key.0, *pressed as i32));
+        }
+
+        let (hat_x, hat_y) = dpad_to_hat(b.dpad_up, b.dpad_down, b.dpad_left, b.dpad_right);
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_HAT0X.0,
+            hat_x,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_HAT0Y.0,
+            hat_y,
+        ));
+
+        let (lx, ly) = self.left_stick.to_evdev_axes();
+        let (rx, ry) = self.right_stick.to_evdev_axes();
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_X.0,
+            lx,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_Y.0,
+            ly,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_RX.0,
+            rx,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_RY.0,
+            ry,
+        ));
+
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_Z.0,
+            self.left_trigger as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_RZ.0,
+            self.right_trigger as i32,
+        ));
+
+        let m = &self.motion;
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_TILT_X.0,
+            m.gyro_x as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_TILT_Y.0,
+            m.gyro_y as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_MISC.0,
+            m.gyro_z as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_BRAKE.0,
+            m.accel_x as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_GAS.0,
+            m.accel_y as i32,
+        ));
+        events.push(InputEvent::new(
+            EventType::ABSOLUTE.0,
+            AbsoluteAxisCode::ABS_WHEEL.0,
+            m.accel_z as i32,
+        ));
+
+        // Flush
+        events.push(InputEvent::new(EventType::SYNCHRONIZATION.0, 0, 0));
+
+        device.emit(&events)?;
+        Ok(())
+    }
 }
 
 impl StickData {
@@ -202,5 +338,10 @@ impl StickData {
         let x = (bytes[0] as u16) | (((bytes[1] & 0x0F) as u16) << 8);
         let y = ((bytes[1] as u16) >> 4) | ((bytes[2] as u16) << 4);
         Self { x, y }
+    }
+
+    pub fn to_evdev_axes(&self) -> (i32, i32) {
+        let scale = |v: u16| -> i32 { ((v as i32 - 2048) * 32767) / 2048 };
+        (scale(self.x), -scale(self.y)) // negate Y
     }
 }
